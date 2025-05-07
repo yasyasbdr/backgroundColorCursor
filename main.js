411606e7fddea0475a27978cc5cb3028eac43d8e
yasyasbdr <yasminebadarou.yb@gmail.com>
2025-05-07 18:21:21 +0200

    premier commit

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#color-range").on("input change", function() {
+        // Change Body Background Color
+        $("body").css("background-color", $(this).val());
+        // Change h1 Color to white
+        if ($("#color-range").val()<="#242424") {
+            $("h1").css("color", "white");
+        } else {
+            $("h1").css("color", "black");
+        }
+    })
+})
